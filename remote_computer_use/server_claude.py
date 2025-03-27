5190954b50d6f4c25c8e43f1f9ee0dcbd3fda8d9
xie river <chuanxie@amazon.com>
2025-03-27 00:31:32 +0000

    add region acpture

--- remote_computer_use/server_claude.py
+++ remote_computer_use/server_claude.py
@@ -108,6 +108,29 @@ def base64_to_pil(base64_str):
     img = Image(data=img_data, format="png")
     return img
 
+@mcp.tool()
+async def capture_region(ctx: Context,x: int, y: int, w: int, h: int) -> Image:
+    """
+    Capture screenshot only represents of a region of the remote desktop
+    
+    Args:
+        x: X coordinate (pixels from the left edge)
+        y: Y coordinate (pixels from the top edge)
+        w: Width of the region
+        h: Hight of the region
+        
+    Returns:
+        Image: Screenshot of the remote desktop
+    """
+    vnc = ctx.request_context.lifespan_context.vnc
+    screenshot = await vnc.capture_region(x,y,w,h)
+    
+    # Convert PIL Image to bytes
+    img_bytes = io.BytesIO()
+    screenshot.save(img_bytes, format="PNG")
+    
+    return Image(data=img_bytes.getvalue(), format="png")
+
 
 @mcp.tool()
 async def computer( ctx: Context,
